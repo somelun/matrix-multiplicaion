1d32049773ce8065259ed453757dbc39150449a8
somelun <somelun@gmail.com>
2022-05-12 22:40:33 +0100

    Fill matrix with random numbers

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 
 int main(int argc, char* argv[]) {
     matrix m(6);
+    m.fill_random();
 
     std::cout << m << std::endl;
 
--- matrix.cpp
+++ matrix.cpp
@@ -1,5 +1,12 @@
 #include "matrix.h"
 
+#include <random>
+#include <iostream>
+#include <iomanip>      // std::setw
+
+constexpr uint16_t MinNum = 1;
+constexpr uint16_t MaxNum = 99;
+
 matrix::matrix(uint16_t dimension) : d(dimension) {
     data = new uint16_t* [d];
     for (uint16_t i = 0; i < d; ++i) {
@@ -22,14 +29,23 @@ uint16_t& matrix::operator()(uint16_t row, uint16_t column) const {
 }
 
 void matrix::fill_random() {
-    //
+    std::random_device rd;
+    // std::default_random_engine eng(rd());
+    std::mt19937 eng(rd());
+    std::uniform_int_distribution<uint16_t> distr(MinNum, MaxNum);
+
+    for (size_t i = 0; i < d; i++) {
+        for (size_t j = 0; j < d; j++) {
+            data[i][j] = distr(eng);
+        }
+    }
 }
 
 std::ostream& operator<<(std::ostream& s, const matrix& m) {
     const uint16_t& d = m.get_dimension();
     for (size_t i = 0; i < d; i++) {
         for (size_t j = 0; j < d; j++) {
-            s << m(i, j) << " ";
+            s << std::setw(2) << m(i, j) << " ";
         }
         s << "\n";
     }
